f8e183878586b70bc40db4f394aa881cdff882c3
santoaajak <santinoaajak@gmail.com>
2026-05-14 19:28:09 +0200

    Add files via upload

---
+++ recursivealgo.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n)
+{
+    if (n == 0) return 1;
+    return n * factorial(n - 1);
+}
+
+int main()
+{
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    cout << n << "! = " << factorial(n) << endl;
+    return 0;
+}
